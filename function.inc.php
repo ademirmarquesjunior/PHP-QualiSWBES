caa76fee47b209cd72f12f26248861c9252e1a20
ademirmarquesjunior <adejunior_marques@hotmail.com>
2016-12-08 03:46:52 -0200

    Testes de gráficos com Chart.js; Refinamento da função para extração dos resultados
    Inclui outras correções menores

--- function.inc.php
+++ function.inc.php
@@ -4,6 +4,7 @@
 function extraiResultados ($form, $objective) {
 	include "conecta.php";
 	$total = 0;
+	$total_weight = 0;
     $counter = 0;
     $sum = array(array(array()));
     $sym_weight = array(array(array()));
@@ -15,7 +16,8 @@ function extraiResultados ($form, $objective) {
 
         while ($linha = mysql_fetch_array($rs)) {
             $total = $total + $linha["tbForm_has_tbUserQuestionAnswer"]*$linha["tbObjectives_has_tbUserWeight"];
-            $counter = $counter + $linha["tbObjectives_has_tbUserWeight"];
+            $total_weight = $total_weight + $linha["tbObjectives_has_tbUserWeight"];
+            $counter++;
             
             if (!isset($sum[$linha["tbArtifact_idtbArtifact"]][$linha["tbCriterion_idtbCriterion"]][$linha["tbSubCriterion_idtbSubCriterion"]])){
             	$sum[$linha["tbArtifact_idtbArtifact"]][$linha["tbCriterion_idtbCriterion"]][$linha["tbSubCriterion_idtbSubCriterion"]] = NULL;
@@ -25,48 +27,85 @@ function extraiResultados ($form, $objective) {
             $sum[$linha["tbArtifact_idtbArtifact"]][$linha["tbCriterion_idtbCriterion"]][$linha["tbSubCriterion_idtbSubCriterion"]] = $sum[$linha["tbArtifact_idtbArtifact"]][$linha["tbCriterion_idtbCriterion"]][$linha["tbSubCriterion_idtbSubCriterion"]] + $linha["tbForm_has_tbUserQuestionAnswer"]*$linha["tbObjectives_has_tbUserWeight"];
             $sum_weight[$linha["tbArtifact_idtbArtifact"]][$linha["tbCriterion_idtbCriterion"]][$linha["tbSubCriterion_idtbSubCriterion"]] = $sum_weight[$linha["tbArtifact_idtbArtifact"]][$linha["tbCriterion_idtbCriterion"]][$linha["tbSubCriterion_idtbSubCriterion"]] + $linha["tbObjectives_has_tbUserWeight"];
         }
-        if ($counter != 0)
-			echo "<p>Atende:";
-			echo "<h2>";
-            printf("%0.2f %%",($total/$counter*20)); 
-            echo "</h2></p>";
-            echo "<p>Questões nessa categoria: ".$counter."</p>";
-        }
+     }
         
-        return array($sum, $sum_weight);
+        return array($sum, $sum_weight, $total, $total_weight, $counter);
 }
 
 function detalhaResultados ($resultados) {
 	$sum = $resultados[0];
 	$sum_weight = $resultados[1];
+	
+	$subcriterion_value = array(array(array()));
+	$subcriterion_name = array(array(array()));	
+	$criterion_value = array(array());
+	$criterion_name = array(array());
+	$artifac_value = array();
+	$artifact_name = array();
+	
     include "conecta.php";
+   
     foreach ($sum as $i=>$valueartifact) {
-    	echo "<p><br>";
-    	$Sql = mysql_query("SELECT * FROM `tbArtifact` WHERE idtbArtifact = ".$i);
-        	while ($rr = mysql_fetch_array($Sql)) {
-        		echo $rr['tbArtifactDescription']."<br> "; 
-        	} 
+    	echo "<p>";
+    	$artifact_sum = 0;
+    	$artifact_weight = 0;
+
     	foreach ($valueartifact as $j=>$valuecriterion) {
-        	$Sql = mysql_query("SELECT * FROM `tbCriterion` WHERE idtbCriterion = ".$j);
-            	while ($rr = mysql_fetch_array($Sql)) {
-            		echo $rr['tbCriterionDesc']."<br> "; 
-				}                           	
-    	
+    		$criterion_sum = 0;
+    		$criterion_weight = 0;
+    		
     		//echo "...".$j."<br>";
     		foreach ($valuecriterion as $k=>$valuesubcriterion) {
     			$Sql = mysql_query("SELECT * FROM `tbSubCriterion` WHERE idtbSubCriterion = ".$k);
             	while ($rr = mysql_fetch_array($Sql)) {
-            		echo $rr['tbSubCriterionDesc']." "; 
+            		//echo $rr['tbSubCriterionDesc']." "; 
+            		$subcriterion = $rr['tbSubCriterionDesc'];
 				}  
     		
-    			$result = $valuesubcriterion/$sum_weight[$i][$j][$k]*20;
-    			printf("%0.2f %%",$result); 
-    			//echo " ".$result;
-    			echo "<br>";
-    			
+    			if ($sum_weight[$i][$j][$k] != 0) {
+	    			$result = $valuesubcriterion/$sum_weight[$i][$j][$k]*20;
+	    			$criterion_sum = $criterion_sum + $valuesubcriterion;
+	    			$criterion_weight = $criterion_weight + $sum_weight[$i][$j][$k];
+	    			$subcriterion_value[$i][$j][$k] = $result;
+	    			$subcriterion_name[$i][$j][$k] = $subcriterion;
+    			}
+    	   	}	
+		
+
+			
+        	$Sql = mysql_query("SELECT * FROM `tbCriterion` WHERE idtbCriterion = ".$j);
+            	while ($rr = mysql_fetch_array($Sql)) {
+            		//echo "<strong>".$rr['tbCriterionDesc']."</strong> "; 
+            		$criterion = $rr['tbCriterionDesc'];
 			}
+			if ($criterion_weight != 0){
+				$result = $criterion_sum/$criterion_weight*20;
+	
+			    $criterion_value[$i][$j] = $result;
+				$criterion_name[$i][$j] = $criterion;
+			}
+						
+	    	$artifact_sum = $artifact_sum + $criterion_sum;
+	    	$artifact_weight = $artifact_weight + $criterion_weight;
+			
 		}
+		
+		
+    	$Sql = mysql_query("SELECT * FROM `tbArtifact` WHERE idtbArtifact = ".$i);
+        	while ($rr = mysql_fetch_array($Sql)) {
+        		//echo "<strong>".$rr['tbArtifactDescription']."</strong> ";
+        		$artifact = $rr['tbArtifactDescription'];
+        } 
+		if ($artifact_weight != 0) {
+			$result = $artifact_sum/$artifact_weight*20;
+			$artifact_name[$i] = $artifact;
+			$artifact_value[$i] = $result;
+		}
+
 	}
+	
+	return array($artifact_value,$artifact_name,$criterion_value,$criterion_name,$subcriterion_value,$subcriterion_name);
+
 }
 
 
--- results.php
+++ results.php
@@ -6,15 +6,16 @@
         <!-- <link rel="stylesheet" href="estilo.css" type="text/css" media="screen" /> -->
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link href="css/style.css" rel="stylesheet">
+        <script src="Chart.min.js"></script>
         <title>SEWebS</title>
     </head>
-
-    <body>
     <?php
-    	include "conecta.php";
-    	include "function.inc.php";
-	?>
-
+    if (!isset($_GET['form']))
+        exit;
+    include "conecta.php";
+    include "function.inc.php";
+    ?>
+    <body>
         <div class="container-fluid">
             <div class="jumbotron">
                 <h2>Modelo de Avaliação de Qualidade dos Sistemas Educacionais
@@ -24,97 +25,145 @@
                 <?php
                 include("valida.php");
                 ?></div>
-                  <div class="panel panel-default">
-                    <div class="panel-body">
-                        <h1>Resultado de avaliação</h1>
-                        Você avaliou <strong>'<?php $Sql = mysql_query("SELECT * FROM tbform INNER JOIN tbapplication ON tbapplication.idtbApplication = tbform.tbApplication_idtbApplication WHERE tbform.idtbForm = ".$_GET['form']); while ($rr = mysql_fetch_array($Sql)) { echo $rr['tbApplicationName']; } ?></strong>'
-                        como <strong>'<?php $Sql = mysql_query("SELECT * FROM `tbUserType` WHERE `idtbUserType` = ".$_SESSION['user_type']); while ($rr = mysql_fetch_array($Sql)) { echo $rr['tbUserTypeDescripton']; } ?>'  
-						</strong>  
-					</div>
-               	 </div>
-
-          
-           <div class="row">
-                <div class="col-md-8">
-                    <h2>Avaliação geral </h2>
-                    <p>Pontuação total considerado todas as questões respondidas </p>
-                    <?php
-                        $total = 0;
-                        $counter = 0;
-                        $form = $_GET['form'];
-                        if ($form != '') {
-                            $Sql = "SELECT * FROM `tbform_has_tbuserquestion` WHERE `tbForm_idtbForm` = " . $form;
-                            $rs = mysql_query($Sql, $conexao) or die("Formulário não existe");
-
-                            while ($linha = mysql_fetch_array($rs)) {
-                                $total = $total + $linha["tbForm_has_tbUserQuestionAnswer"];
-                                $counter++;
-                            }
-                            if ($counter != 0)
-								echo "<h2>";
-								printf("%0.2f %%",($total/$counter*20));
-								echo "</h2>";
-                                echo "<br>Questões nessa categoria: ".$counter;
-                            }
-                        ?><a class="btn" href="#">Detalhes »</a>
-                </div>
-                <div class="col-md-8">
-                    <h2>Entrega+Distribuição - Conteúdo </h2>
-                    <p>Conteúdo distribuído e descentralizado na Web, mas ligados por ontologias, 
-                        e é entregue pelo sistema de acordo com o perfil do usuário e seus tópicos 
-                        de interesse. </p>
-                        <?php
-                            $detalhes = array();
-                        	$detalhes = extraiResultados($_GET['form'], 2);
-                        	detalhaResultados($detalhes); 
-                        ?>
-                        
-                        
-                    <p><a class="btn" href="#">Detalhes »</a> </p>
-                </div>
-                <div class="col-md-8">
-                    <h2>Acesso - Conteúdo e Sistema </h2>
-                    <p>Acesso não linear, e expandido – estudante descreve uma situação 
-                        e realiza consultas semânticas sobre um conteúdo adequado (de acordo 
-                        com contexto da aprendizagem e perfil do usuário). </p>
-                        <?php
-                        	$detalhes = array();
-                        	$detalhes = extraiResultados($_GET['form'], 4);
-                        	detalhaResultados($detalhes);                        	
-                        ?>
-
-                    <p><a class="btn" href="#">Detalhes »</a> </p>
-                </div>
-                <div class="col-md-8">
-                    <h2>Capacidade de Resposta - Sistema </h2>
-                    <p>Sistema responde de forma proativa com materiais de aprendizagem 
-                        de acordo com o contexto do problema atual (cada usuário tem seu agente 
-                        personalizado que se comunica com outros agentes). </p>
-                        <?php
-                        	$detalhes = array();
-                        	$detalhes = extraiResultados($_GET['form'], 1);
-                        	detalhaResultados($detalhes);                         ?>
-
-                    <p><a class="btn" href="#">Detalhes »</a> </p>
-                </div>
-                <div class="col-md-8">
-                    <h2>Dinâmica da Aprendizagem - Aprendizagem
-                    </h2>
-                    <p>Todas as atividades são integradas (ambiente de negócio integrado 
-                        - ex.: sistema de notas, matrícula, faltas, cursos já realizados) – 
-                        aprendizagem ativa, estimulada pelo sistema por meio dos agentes inteligentes.
-                    </p>
-                        <?php
-                        	$detalhes = array();
-                        	$detalhes = extraiResultados($_GET['form'], 3);
-                        	detalhaResultados($detalhes);                         ?>
-
-                    <p><a class="btn" href="#">Detalhes »</a> </p>
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <h1>Resultado de avaliação</h1>
+                    Você avaliou <strong>'<?php $Sql = mysql_query("SELECT * FROM tbform INNER JOIN tbapplication ON tbapplication.idtbApplication = tbform.tbApplication_idtbApplication WHERE tbform.idtbForm = " . $_GET['form']);
+                $i = 0;
+                while ($rr = mysql_fetch_array($Sql)) {
+                    echo $rr['tbApplicationName'];
+                    $i++;
+                } if (!($i)) header('Location:index2.php'); ?></strong>'
+                    como <strong>'<?php $Sql = mysql_query("SELECT * FROM `tbUserType` WHERE `idtbUserType` = " . $_SESSION['user_type']);
+                while ($rr = mysql_fetch_array($Sql)) {
+                    echo $rr['tbUserTypeDescripton'];
+                } ?>'  
+                    </strong>  
                 </div>
             </div>
-            <div id="footer" class="well well-sm">
-                Desenvolvimento: Ademir Marques Junior - 2016 </div>
-        </div>
-    </body>
 
-</html>
+
+            <?php
+            $resultados = array();
+            $values = array();
+            $names = array();
+            $resultados_exp = array();
+
+            $Sql = "SELECT * FROM `tbobjectives`";
+            $rs = mysql_query($Sql, $conexao) or die("Erro na pesquisa");
+            while ($linha = mysql_fetch_array($rs)) {
+                $names[$linha["idtbObjectives"] - 1] = $linha["tbObjectivesDesc"];
+
+                $resultados[$linha["idtbObjectives"]] = array();
+                $resultados[$linha["idtbObjectives"]] = extraiResultados($_GET['form'], $linha["idtbObjectives"]);
+                $value = $resultados[$linha["idtbObjectives"]][2] / $resultados[$linha["idtbObjectives"]][3] * 20;
+                $values[$linha["idtbObjectives"] - 1] = $value;
+                $resultados_exp[$linha["idtbObjectives"]] = detalhaResultados($resultados[$linha["idtbObjectives"]]);
+            }
+            ?>
+            <div class="panel panel-default">
+                <div class="panel-body">
+
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h2>Visão Geral </h2>
+                            <p>A aplicação obteve a média geral de <h3><?php printf("%0.2f %% :", array_sum($values) / count(array_filter($values))); ?></h3> distribuídos entre os objetivos abaixo: </p>
+                            <canvas id="GraficoBarra" style="width:100%;"></canvas>
+                            <?php
+                            $loadgraph = '';
+
+                            echo '
+		            <script type="text/javascript">
+                var options = { responsive:true, scaleOverride:true, scaleSteps:10, scaleStartValue:0, scaleStepWidth:10, display:true };
+
+                var data = {
+                    labels: [';
+                            foreach ($names as $name) {
+                                echo '"' . $name . '",';
+                            }
+                            echo '],
+                    datasets: [
+                        {
+                            label: "Dados primários",
+                            fillColor: "rgba(220,220,220,0.5)",
+                            strokeColor: "rgba(220,220,220,0.8)",
+                            highlightFill: "rgba(220,220,220,0.75)",
+                            highlightStroke: "rgba(220,220,220,1)",
+                            data: [';
+                            foreach ($values as $value) {
+                                echo json_encode($value) . ',';
+                            }
+                            echo ']
+                        }
+                    ]
+                }</script>;';
+
+                            $loadgraph = $loadgraph . 'var ctx = document.getElementById("GraficoBarra").getContext("2d");
+                    var BarChart = new Chart(ctx).Bar(data, options);';
+                            ?>
+
+                        </div>
+					</div>
+				</div>
+			</div>
+                       <div class="row">
+                             <?php
+                                for ($i = 1; $i < 5; $i++) {
+                                    $artifact_value = $resultados_exp[$i][0];
+                                    $artifact_name = $resultados_exp[$i][1];
+                                    echo ' <div class="col-md-6"><div class="panel panel-default"><div class="panel-body">
+';
+                                    echo '<h2>' . $names[$i-1] ." ";
+                                    printf("%0.2f%% ", $values[$i-1]);
+                                    echo '</h2>';
+                                    echo '<p>Explicação curta sobre o objetivo. Obs: talvez fazer uma entrada para esse texto no banco de dados. Obs2: Ler esse texto do banco de dados devido à repetição</p>';
+                                    echo 'Os seguintes Artefatos foram avaliados para alcançar este objetivos. Cada artefato ainda é detalhado em fatores e subfatores avaliados:';
+                                    echo '<canvas id="GraficoArtefato' . $i . '" style="width:100%;"></canvas>';
+                                    echo '
+		            <script type="text/javascript">
+                var options' . $i . ' = { responsive:true, scaleOverride:true, scaleSteps:10, scaleStartValue:0, scaleStepWidth:10, display:true };
+
+                var data' . $i . ' = {
+                    labels: [';
+                                    foreach ($artifact_name as $name) {
+                                        echo '"' . $name . '",';
+                                    }
+                                    echo '],
+                    datasets: [
+                        {
+                            label: "Dados primários",
+                            fillColor: "rgba(220,220,220,0.5)",
+                            strokeColor: "rgba(220,220,220,0.8)",
+                            highlightFill: "rgba(220,220,220,0.75)",
+                            highlightStroke: "rgba(220,220,220,1)",
+                            data: [';
+                                    foreach ($artifact_value as $value) {
+                                        echo json_encode($value) . ',';
+                                    }
+                                    echo ']
+                        }
+                    ]
+                };</script>';
+                                    $loadgraph = $loadgraph . 'var ctx' . $i . ' = document.getElementById("GraficoArtefato' . $i . '").getContext("2d");
+                 var BarChart' . $i . ' = new Chart(ctx' . $i . ').Bar(data' . $i . ', options' . $i . ');';
+                 
+                 			echo '</div></div></div>';
+                                }
+                                ?>
+                        </div>
+                        <div id="footer" class="well well-sm">
+                            Desenvolvimento: Ademir Marques Junior - 2016 </div>
+                    </div>
+                    <?php
+                    echo '<script>
+         window.onload = function(){
+          ' . $loadgraph . '     
+          }           
+         </script>';
+                    ?>        
+
+
+
+                    </body>
+
+                    </html>
